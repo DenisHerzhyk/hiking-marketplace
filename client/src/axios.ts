b33cc2ad39ae0c4a3807d3136af9dc5b8026866e
DenisHerzhyk <denis.herzhyk88@gmail.com>
2026-07-04 02:26:22 +0300

    fixed axios.ts

--- client/src/axios.ts
+++ client/src/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:4996",
+  baseURL: import.meta.env.VITE_RENDER_URL || "http://localhost:4996",
   withCredentials: true,
 });
 
