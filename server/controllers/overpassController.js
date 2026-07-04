359762226a058e38343b25b4d0153ca55f2420f3
DenisHerzhyk <denis.herzhyk88@gmail.com>
2026-07-04 21:10:32 +0300

    changed send to text

--- server/controllers/overpassController.js
+++ server/controllers/overpassController.js
@@ -11,7 +11,7 @@ export const overpassQuery = async (req, res) => {
     const text = await response.text();
     console.log("Raw Overpass response:", text.slice(0, 500));
 
-    return res.send(data);
+    return res.send(text);
   } catch (err) {
     console.log("Overpass error:", err);
     return res.status(500).json({ message: "Overpass query failed" });
