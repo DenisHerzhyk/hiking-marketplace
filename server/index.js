eace724003edf260d529982b5c925cc00f1842a1
DenisHerzhyk <denis.herzhyk88@gmail.com>
2026-08-28 21:03:56 +0300

    fixed the stock managing with adding verification

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:5174", "https://hiking-ecommerce.vercel.app"],
+    origin: ["http://localhost:5173", "https://hiking-ecommerce.vercel.app"],
     credentials: true,
   }),
 );
